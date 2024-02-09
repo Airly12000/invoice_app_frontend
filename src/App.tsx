47fde000be521afdcb53fc64a557cd74a7e62716
Airly12000 <littlefrancis28@gmail.com>
2024-02-09 12:57:58 +0000

    migration

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import SideBar from "./components/SideBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Invoices from "./pages/Invoices";
+import InvoiceDetails from "./pages/InvoiceDetails";
+import NotFound from "./pages/NotFound";
+
+export const Context = React.createContext<any>({});
 
 function App() {
+  const [openModal, setOpenModal] = useState(false);
+  const [modalTitle, setModalTitle] = useState("Confrim");
+  const [modalBody, setModalBody] = useState("Are you sure");
+  const [modalButton, setModalButton] = useState("primary");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container-fluid" id="mainBody">
+        <div className="row">
+          <SideBar />
+          <div className="col d-flex flex-column vh-90 position-relative">
+            <div className="row">
+              <div className="col-md-1 col-xl-2"></div>
+              <div className="col-md col-lg">
+                <Context.Provider
+                  value={{
+                    openModal,
+                    setOpenModal,
+                    modalTitle,
+                    modalBody,
+                    modalButton,
+                    setModalTitle,
+                    setModalBody,
+                    setModalButton,
+                  }}
+                >
+                  <Router>
+                    <Routes>
+                      <Route path="/" element={<Invoices />} />
+                      <Route path="/invoice/:id" element={<InvoiceDetails />} />
+                      <Route path="*" element={<NotFound />} />
+                    </Routes>
+                  </Router>
+                </Context.Provider>
+              </div>
+              <div className="col-md-1 col-xl-2"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
